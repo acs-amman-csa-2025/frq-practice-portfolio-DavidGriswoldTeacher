9cc5e9a7be156865f1f4384eddc7638c14d9ef1a
David Griswold <dgriswold@acsamman.edu.jo>
2026-02-03 09:55:29 +0300

    problem 2

---
+++ docs/prog402a.md
@@ -0,0 +1,16 @@
+## Problem statement and definition
+
+Read 21 pairs of numbers (ID number and score respectively) into two separate arrays.  Find the average score.  Print a table as shown below of the ID, score and difference (score - average) for each student, one student per line.  Print the 
+average at the end as shown.c
+
+## Things I did or review to prepare for this problem
+
+Reminded myself how Scanners and File objects work
+
+## Problems I faced doing this problem
+
+the average on the site and the average i calculated were different, had to figure out why - it was their fault
+
+## Things I learned doing this problem
+
+you can reuse the same integer variable for all loops
---
+++ src/prog402a.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class prog402a {
+
+    public static void main(String[] args) throws Exception {
+        File f = new File("data/prog402a.txt");
+        Scanner in = new Scanner(f);
+        int[] ids = new int[21];
+        int[] scores = new int[21];
+        for (int i = 0; i < 21; i++) {
+            ids[i] = in.nextInt();
+            scores[i] = in.nextInt();
+        }
+
+        double sum = 0.0;
+        for (int i = 0; i < 21; i++) {
+            sum += scores[i];
+        }
+        double avg = sum / 21;
+
+        for (int i = 0; i < 21; i++) {
+            System.out.println(ids[i] + " " + scores[i] + " " + (scores[i] - avg));
+        }
+        System.out.println(avg);
+    }
+}
